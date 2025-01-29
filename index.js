125575fefdafe0bbe25391dd6c07c48962a238de
woopackerpat <p.taechakraichana@gmail.com>
2025-01-29 12:32:28 +0700

    day2

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Send post list" });
+app.get("/post/list", (req, res) => {
+  res.status(200).json({ message: "Send post list" });
 });
+
+app.get("/post/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.json({ postId: id });
+});
+
+// app.get("/product/price/:price/discount/:discount", (req, res) => {
+//   const { price, discount } = req.params;
+
+//   res.json({ price, discount });
+// });
 
 app.post("/", (req, res) => {
   res.json({ message: "Create post" });
